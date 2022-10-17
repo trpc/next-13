b9d91717a163dcdcaf5cb98abfca382ba76906da
KATT <alexander@n1s.se>
2022-10-17 23:17:30 +0200

    add error

---
+++ app/secret/error.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <p style={{ border: "1px solid red", padding: "4px" }}>
+      Something went wrong - {error.message}
+    </p>
+  );
+}
