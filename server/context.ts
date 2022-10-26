8f60b48c22f5fb865f98fe0169a4afc69d770d29
KATT <alexander@n1s.se>
2022-10-26 02:47:33 +0200

    fix it

--- server/context.ts
+++ server/context.ts
@@ -2,7 +2,6 @@
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 import { unstable_getServerSession } from "next-auth";
-import { headers } from "next/headers";
 import { getUser, User } from "~/app/_lib/getUser";
 import { nextAuthOptions } from "~/pages/api/auth/[...nextauth]";
 
@@ -22,23 +21,26 @@ export async function createContextInner(opts: CreateContextOptions) {
   };
 }
 
-export type Context = trpc.inferAsyncReturnType<typeof createContextInner>;
-
 /**
  * Creates context for an incoming request
  * @link https://trpc.io/docs/context
  */
 export async function createContext(
-  opts?: trpcNext.CreateNextContextOptions,
-): Promise<Context> {
+  opts: // HACKs because we can't import `next/cookies` in `/api`-routes
+  | {
+        type: "rsc";
+        getUser: typeof getUser;
+      }
+    | (trpcNext.CreateNextContextOptions & { type: "api" }),
+) {
   // for API-response caching see https://trpc.io/docs/caching
 
-  if (!opts) {
+  if (opts.type === "rsc") {
     // RSC
-    return createContextInner({
-      rsc: true,
-      user: await getUser(),
-    });
+    return {
+      type: opts.type,
+      user: await opts.getUser(),
+    };
   }
   // not RSC
   const session = await unstable_getServerSession(
@@ -46,10 +48,10 @@ export async function createContext(
     opts.res,
     nextAuthOptions,
   );
-
-  // FIXME fix auth for api requests
-  return await createContextInner({
-    rsc: false,
-    user: null,
-  });
+  return {
+    type: opts.type,
+    user: session,
+  };
 }
+
+export type Context = trpc.inferAsyncReturnType<typeof createContext>;
