5cfb43b96b22874dc1d97193ce1454a964a99ef7
KATT <alexander@n1s.se>
2022-10-28 01:40:32 +0200

    cleanup

--- client/trpcClient.tsx
+++ client/trpcClient.tsx
@@ -1,10 +1,9 @@
 "use client";
-import { createTRPCProxyClient, httpBatchLink, loggerLink } from "@trpc/client";
-import { createTRPCReact } from "@trpc/react-query";
-import type { AppRouter } from "~/server/routers/_app";
-import superjson from "superjson";
-import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink, loggerLink } from "@trpc/client";
+import { createTRPCReact } from "@trpc/react-query";
+import superjson from "superjson";
+import type { AppRouter } from "~/server/routers/_app";
 
 export const trpc = createTRPCReact<AppRouter>({
   unstable_overrides: {
