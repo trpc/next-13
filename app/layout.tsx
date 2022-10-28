48b1fa7ea5cd096f3dc6351ce7ae1aacd3367929
KATT <alexander@n1s.se>
2022-10-28 02:50:12 +0200

    client provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { ReactNode } from "react";
+import { ClientProvider } from "~/client/trpcClient";
 import { RefreshButton } from "../client/RefreshButton";
 import { rsc } from "../server-rsc/trpc";
 
@@ -11,40 +12,42 @@ interface Props {
 export default function RootLayout(props: Props) {
   const user = rsc.whoami.use();
   return (
-    <html lang='en'>
-      <head>
-        <title>Next.js hello</title>
-      </head>
-      <body>
-        <nav>
-          <ul>
-            <li>
-              <Link href='/'>Home</Link>
-            </li>
-            <li>
-              Logged in as{" "}
-              {user ? (
-                <>
-                  {user.name} - <Link href='/api/auth/signout'>Logout</Link>
-                </>
-              ) : (
-                <>
-                  Not logged in - login{" "}
-                  <Link href='/api/auth/signin'>Login</Link>
-                </>
-              )}
-            </li>
-            <li>
-              <Link href='/secret'>Secret page</Link>
-            </li>
-            <li>
-              <RefreshButton />
-            </li>
-          </ul>
-        </nav>
+    <ClientProvider>
+      <html lang='en'>
+        <head>
+          <title>Next.js hello</title>
+        </head>
+        <body>
+          <nav>
+            <ul>
+              <li>
+                <Link href='/'>Home</Link>
+              </li>
+              <li>
+                Logged in as{" "}
+                {user ? (
+                  <>
+                    {user.name} - <Link href='/api/auth/signout'>Logout</Link>
+                  </>
+                ) : (
+                  <>
+                    Not logged in - login{" "}
+                    <Link href='/api/auth/signin'>Login</Link>
+                  </>
+                )}
+              </li>
+              <li>
+                <Link href='/secret'>Secret page</Link>
+              </li>
+              <li>
+                <RefreshButton />
+              </li>
+            </ul>
+          </nav>
 
-        <main>{props.children}</main>
-      </body>
-    </html>
+          <main>{props.children}</main>
+        </body>
+      </html>
+    </ClientProvider>
   );
 }
