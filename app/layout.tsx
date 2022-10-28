926117512cad99c33c7de79e68fccf54ff4dd108
Alex / KATT <alexander@n1s.se>
2022-10-28 11:58:22 +0200

    fix viewport (#17)

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,10 @@ export default function RootLayout(props: Props) {
       <html lang='en'>
         <head>
           <title>Next.js hello</title>
+          <meta
+            name='viewport'
+            content='width=device-width, initial-scale=1.0'
+          />
         </head>
         <body>
           <nav className='p-4'>
