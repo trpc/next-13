d871e0ef3bd4c713d3d50b87d6cda28a73fc3e65
KATT <alexander@n1s.se>
2022-11-01 23:06:42 +0100

    fix error

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,8 +1,9 @@
+import { use } from "react";
 import { rsc } from "~/server-rsc/trpc";
 
 type FIXMEType = any;
 export default function Page(props: FIXMEType) {
-  const post = rsc.post.byId.use({ id: props.params.id });
+  const post = use(rsc.post.byId.fetch({ id: props.params.id }));
 
   return (
     <div className='p-4'>
