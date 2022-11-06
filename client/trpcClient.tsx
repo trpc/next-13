99c61e108ecc761896906b416b5cd3e7d4254a5c
KATT <alexander@n1s.se>
2022-11-06 23:30:24 +0100

    test now hyd

--- client/trpcClient.tsx
+++ client/trpcClient.tsx
@@ -17,6 +17,20 @@ export const trpc = createTRPCReact<AppRouter>({
   },
 });
 
+function getBaseUrl() {
+  if (typeof window !== "undefined")
+    // browser should use relative path
+    return "";
+  if (process.env.VERCEL_URL)
+    // reference for vercel.com
+    return `https://${process.env.VERCEL_URL}`;
+  if (process.env.RENDER_INTERNAL_HOSTNAME)
+    // reference for render.com
+    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
+  // assume localhost
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
 export function ClientProvider(props: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
@@ -26,7 +40,7 @@ export function ClientProvider(props: { children: React.ReactNode }) {
           enabled: () => true,
         }),
         httpBatchLink({
-          url: "/api/trpc",
+          url: `${getBaseUrl()}/api/trpc`,
         }),
       ],
       transformer: superjson,
